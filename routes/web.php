0f790107ae895af5e94ca8166e8c1e2f14cbfe96
acilegna <acilegna.airam88@gmail.com>
2024-11-24 22:59:11 -0600

    update archivo routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,12 @@ Route::get('/tasks', [TasksController::class, 'index'])->name('tasks');
 Route::post('/newtasks', [TasksController::class, 'store'])->name('newtasks');
 Route::put('/updatetasks/{id}', [TasksController::class, 'update'])->name('updatetasks');
 Route::delete('/deletetasks/{id}', [TasksController::class, 'destroy'])->name('deletetasks');
+
+ 
+ 
+/* 
+URL Api
+http: //127.0.0.1:8000/tasks
+http: //127.0.0.1:8000/newtasks
+http: //127.0.0.1:8000/updatetasks/5
+http://127.0.0.1:8000/deletetasks/11 */
